e51e4e63b81b254e95f73091977c8668d50528c4
BeauConstrictor <scabby-gone-boring@duck.com>
2025-10-30 19:35:29 +0000

    to help with experiements in the hex monitor, there is now a dir containing memory addresses for every program

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -e
 for f in programs/*; do
     name=$(basename "$f")
     echo -e "\033[32mAssembling $name...\033[0m"
-    vasm -Fbin -dotdir -o "roms/${name%.*}.bin" "$f"
+    vasm -Fbin -dotdir -o "roms/${name%.*}.bin" "$f" -L "apis/${name%.*}.txt"
     echo
 done
+
