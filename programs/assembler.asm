72e3d4dfc793894a3e627e240a3bd3603ba07750
BeauConstrictor <scabby-gone-boring@duck.com>
2025-10-31 20:22:21 +0000

    real first version of the assembler

--- programs/assembler.asm
+++ programs/assembler.asm
@@ -1,27 +1,27 @@
   .org $8003
 
 ; memory map:
-SERIAL = $8002
-
-; monitor subroutines:
-; TODO: include copies of these in this program
-get_key  = $c046
-hex_byte = $c13b
-get_byte = $c14c
+SERIAL   = $8002
+EXIT_VEC = $fff8
 
 ; ascii codes:
 NEWLINE = $0a
+ESCAPE  = $1b
 
 ; memory allocation:
-opc_buf     = $20    ; 3 bytes 
-opc_handler = $23    ; 2 bytes
-insert_ptr  = $25    ; 2 bytes
+BYTE_BUILD  = $40     ; 1 byte
+PRINT       = $50     ;  1byte
+opc_buf     = $20     ; 3 bytes 
+opc_handler = $23     ; 2 bytes
+insert_ptr  = $30     ; 2 bytes (matches the monitor)
 
 main:
-  lda #02
-  sta insert_ptr+1
-  lda #00
-  sta insert_ptr
+  lda #start_msg
+  sta PRINT
+  lda #>start_msg
+  sta PRINT + 1
+  jsr print
+
 mainloop:
   lda #NEWLINE
   sta SERIAL
@@ -174,6 +174,7 @@ lda_handler:
   jsr get_val     ; insert the immediate value
   jsr insert_a
   rts
+
 sta_handler:
   lda #$8d
   jsr insert_a
@@ -195,6 +196,136 @@ bad_handler:
   lda #"("
   sta SERIAL
   jmp mainloop
+
+; return (in a) a single key, ignoring spaces
+; modifies: a, x, y
+get_key:
+  lda SERIAL
+  beq get_key  ; if no char was typed, check again.
+
+  cmp #ESCAPE  ; if esc was pressed,
+  beq _get_key_escape ; exit.
+  cmp #"@"     ; if "@" was pressed,
+  beq _get_key_new_addr  ; move to a new address.
+
+  sta SERIAL   ; echo back the char.
+
+  cmp #" "     ; if space was pressed,
+  beq get_key  ; wait for the next key.
+
+  rts
+_get_key_escape:
+  lda #end_msg
+  sta PRINT
+  lda #>end_msg
+  sta PRINT + 1
+  jsr print
+  jmp (EXIT_VEC)
+_get_key_new_addr:
+  lda #NEWLINE
+  sta SERIAL
+  sta SERIAL
+
+  jsr get_byte
+  sta insert_ptr+1
+  jsr get_byte
+  sta insert_ptr
+
+  lda #":"
+  sta SERIAL
+  lda #" "
+  sta SERIAL
+
+  jmp get_key
+
+; wait for a key and return (in a) the value of a single hex char
+; modifies: a (duh)
+get_nibble:
+  jsr get_key
+  cmp #$3a
+  bcc _get_nibble_digit
+  sec
+  sbc #"a" - 10
+  rts
+_get_nibble_digit:
+  sbc #"0" - 1
+  rts
+
+; return (in a) the a register as hex
+; modifies: a (duh)
+hex_nibble:
+  cmp #10
+  bcc _hex_nibble_digit
+  clc
+  adc #"a" - 10
+  rts
+_hex_nibble_digit:
+  adc #"0"
+  rts
+
+; return (in x & y) the a register as hex
+; modifies: x, y, a
+hex_byte:
+  pha ; save the full value for later
+  ; get just the MSN
+  lsr
+  lsr
+  lsr
+  lsr
+  jsr hex_nibble
+  tax ; but the hex char for the MSN in x
+
+  pla ; bring back the full value
+  and #$0f ; get just the LSN
+  jsr hex_nibble
+  tay ; but the hex char for the LSN in y
+
+  rts
+
+; wait for a key and return (in a) the value of a byte (2 hex chars)
+; modifies: a (duh)
+get_byte:
+  ; get the MS nibble and move it to the MS area of the a reg
+  jsr get_nibble
+  asl
+  asl
+  asl
+  asl
+  ; move the MSN to memory
+  sta BYTE_BUILD
+
+  ; get the LSN and combine it with the MSN
+  jsr get_nibble
+  ora BYTE_BUILD
+  rts
+
+; write the address of a null-terminated string to PRINT
+; modifies: a
+print:
+  tya
+  pha
+  ldy #0
+_print_loop:
+  lda (PRINT),y
+  beq _print_done
+  sta SERIAL
+  iny
+  jmp _print_loop
+_print_done:
+  pla
+  tay
+  rts
+
+start_msg:
+  .byte NEWLINE, NEWLINE
+  .byte "(mnemonic mode)"
+  .byte 0
+
+end_msg:
+  .byte NEWLINE, NEWLINE
+  .byte "(normal mode)"
+  .byte NEWLINE
+  .byte 0
 
 ; note to self: make sure to order these roughly by usage, so that
 ; dispatching is faster
